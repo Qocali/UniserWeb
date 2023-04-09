b3bb0e4e53a5951499c837426b20f8c764699121
Qoceli Baba <babayevqocheli@gmail.com>
2023-04-09 17:04:34 +0400

    Add project files.

---
+++ UniserWeb/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UniserWeb
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection("data source=DESKTOP-GIMUK1I\\SQLEXPRESS;database=Uniser;integrated security=SSPI;"))
+            {
+                SqlDataAdapter sde = new SqlDataAdapter("Select * from Products", con);
+                DataSet ds = new DataSet();
+                sde.Fill(ds);
+                DataGrid1.DataSource = ds;
+                DataGrid1.DataBind();
+            }
+        }
+       
+    }
+}
