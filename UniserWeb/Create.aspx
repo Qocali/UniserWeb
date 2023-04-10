dd5171292f8f21174783459cc9817f5b9060cd74
Qoceli Baba <babayevqocheli@gmail.com>
2023-04-10 11:18:07 +0400

    update create.asax

--- UniserWeb/Create.aspx
+++ UniserWeb/Create.aspx
@@ -14,11 +14,19 @@
 <asp:TextBox ID="UserName" runat="server" ToolTip="Enter User Name"></asp:TextBox>  
              <asp:Label ID="label1" runat="server">SubName</asp:Label>  
 <asp:TextBox ID="SubName" runat="server" ToolTip="Enter User Name"></asp:TextBox> 
+             <asp:Label ID="label4" runat="server">Details</asp:Label>  
+            <asp:TextBox ID="TextBox2" runat="server" ToolTip="Enter Details"></asp:TextBox> 
              <asp:Label ID="label2" runat="server">Price</asp:Label>  
+            <asp:TextBox ID="TextBox1" runat="server" ToolTip="Enter Price"></asp:TextBox> 
              <asp:Label ID="label3" runat="server">Year</asp:Label>  
             <asp:TextBox ClientIDMode="Static" ID="Date" runat="server" CssClass="calendarImg" Width="100px" type="date"></asp:TextBox>
 <asp:TextBox ID="Price" runat="server" ToolTip="Enter User Name"></asp:TextBox>  
             <p>Browse to Upload File</p>  
+             <select runat="server" style="margin-bottom:40px;margin-top:30px;" id="category" class="form-control" name="CatId">
+         <option>Bmw</option>
+         <option>Audi</option>
+
+        </select>
             
         </div>  
         <p>  
--- UniserWeb/Create.aspx.designer.cs
+++ UniserWeb/Create.aspx.designer.cs
@@ -69,6 +69,24 @@ namespace UniserWeb
         protected global::System.Web.UI.WebControls.TextBox SubName;
 
         /// <summary>
+        /// label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label label4;
+
+        /// <summary>
+        /// TextBox2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+
+        /// <summary>
         /// label2 control.
         /// </summary>
         /// <remarks>
@@ -76,6 +94,15 @@ namespace UniserWeb
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label label2;
+
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
 
         /// <summary>
         /// label3 control.
@@ -103,6 +130,15 @@ namespace UniserWeb
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox Price;
+
+        /// <summary>
+        /// category control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlSelect category;
 
         /// <summary>
         /// Button1 control.
